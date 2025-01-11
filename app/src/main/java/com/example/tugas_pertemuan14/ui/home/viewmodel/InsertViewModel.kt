a7971688a765fa8c31e74e5310290e2f8e7276e7
defarrel <Defarrel>
2025-01-11 18:46:15 +0700

    Membuat InsertViewModel dan sealed class FormState

--- app/src/main/java/com/example/tugas_pertemuan14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/tugas_pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -1,6 +1,69 @@
 package com.example.tugas_pertemuan14.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.focus.FocusManager
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.tugas_pertemuan14.model.Mahasiswa
+import com.example.tugas_pertemuan14.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+): ViewModel(){
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    //Validasi data input form
+    fun validateFields(): Boolean{
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if(event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if(event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if(event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if(event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if(event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if(event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs(){
+        if(validateFields()){
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    repositoryMhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Berhasil menambahkan data mahasiswa")
+                }catch (e: Exception){
+                    uiState = FormState.Error("Gagal menambahkan data mahasiswa: ${e.message}")
+                }
+            }
+        }else{
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+    fun resetForm(){
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+    fun resetSnackBarMessage(){
+        uiState = FormState.Idle
+    }
+}
+
+sealed class FormState{
+    object Idle: FormState()
+    object Loading: FormState()
+    data class Success(val message: String): FormState()
+    data class Error(val message: String): FormState()
+}
 
 data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
@@ -37,11 +100,14 @@ data class MahasiswaEvent(
 )
 
 //Menyimpan input form ke dalam entity
-fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa{
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
     jenisKelamin = jenisKelamin,
     alamat = alamat,
     kelas = kelas,
     angkatan = angkatan
-}
+)
+
+
+
