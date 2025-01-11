87396bbcfcaa2d432d41d0f63a48c79799518d75
defarrel <Defarrel>
2025-01-11 18:08:17 +0700

    Menambahkan fungsi getmhs dan insertmhs di NetworkRepositoryMhs

--- app/src/main/java/com/example/tugas_pertemuan14/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/tugas_pertemuan14/repository/NetworkRepositoryMhs.kt
@@ -6,12 +6,19 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs (
     private val fireStore: FirebaseFirestore
 ): RepositoryMhs{
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try{
+            fireStore.collection("Mahasiswa")
+                .add(mahasiswa)
+                .await()
+        }catch (e: Exception){
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
@@ -23,15 +30,25 @@ class NetworkRepositoryMhs (
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)
                     }
-                    trySend(mhsList)
+                    trySend(mhsList) //try send memberikan fungsi untuk mengirimkan data ke flow
                 }
             }
         awaitClose()
         mhsCollection.remove()
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow{
+        val mhsDocumented = fireStore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener{value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose{
+            mhsDocumented.remove()
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
