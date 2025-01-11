b51612db975799683626e60df119717172a06954
defarrel <Defarrel>
2025-01-11 19:19:49 +0700

    Menambahkan fungsi InsertBodyMhs di InsertView

--- app/src/main/java/com/example/tugas_pertemuan14/ui/home/pages/InsertView.kt
+++ app/src/main/java/com/example/tugas_pertemuan14/ui/home/pages/InsertView.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -25,7 +29,45 @@ import com.example.tugas_pertemuan14.ui.home.viewmodel.HomeUiState
 import com.example.tugas_pertemuan14.ui.home.viewmodel.InsertUiState
 import com.example.tugas_pertemuan14.ui.home.viewmodel.MahasiswaEvent
 
+@Composable
+fun InsertBodyMhs(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading
 
+        )   {
+            if(homeUiState is FormState.Loading){
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier
+                        .size(20.dp)
+                        .padding(end = 8.dp)
+                )
+                Text("Loading....")
+            }else{
+                Text("Add")
+            }
+        }
+    }
+}
 
 @Composable
 fun FormMahasiswa(
