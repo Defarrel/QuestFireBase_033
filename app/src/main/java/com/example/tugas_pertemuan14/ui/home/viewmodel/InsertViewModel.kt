67ce3f3b41d4e18afaf4879723d27c331de73f19
defarrel <Defarrel>
2025-01-11 18:27:53 +0700

    Membuat data class MahasiswaEvent, FormErrorState, InsertUiState dan funsgi MahasiswaEvent di InsertViewModel

---
+++ app/src/main/java/com/example/tugas_pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.tugas_pertemuan14.ui.home.viewmodel
+
+import com.example.tugas_pertemuan14.model.Mahasiswa
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val isLoading: Boolean = false
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+){
+    fun isValid(): Boolean{
+        return nim == null
+                && nama == null
+                && jenisKelamin == null
+                && alamat == null
+                && kelas == null
+                && angkatan == null
+    }
+}
+
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+//Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa{
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+}
